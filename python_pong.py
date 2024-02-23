9716f16cb9af58e59674355811087b79152dd2f7
Caspacho111 <casparwilson111@gmail.com>
2024-02-23 18:16:33 +0000

    described window

---
+++ python_pong.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init()
+
+#INITIALS
+WIDTH, HEIGHT= 1000, 600
+wn = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Python_Pong")
+run = True
+
+#colours
+ORANGE = (255, 165, 0)
+
+#for the ball
+radius = 15
+ball_x, balll_y = WIDTH/2 - radius, HEIGHT/2 - radius
+
+#Main Loop
+while run:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            run = False
+
+    pygame.draw.circle(wn, ORANGE, (ball_x, ball_y), radius)
