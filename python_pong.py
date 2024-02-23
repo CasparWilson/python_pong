c8fef1b1fb622ebce77ccccc9398262343d36597
Caspacho111 <casparwilson111@gmail.com>
2024-02-23 18:25:46 +0000

    Paddles described

--- python_pong.py
+++ python_pong.py
@@ -10,10 +10,16 @@ run = True
 
 #colours
 ORANGE = (255, 165, 0)
+BLUE = (0, 0, 255)
 
 #for the ball
 radius = 15
 ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
+
+#paddle dimensions
+paddle_width, paddle_height = 20,120
+left_paddle_y = right_paddle_y = HEIGHT/2 - paddle_height/2
+left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH -(100 - paddle_width/2)
 
 #Main Loop
 while run:
@@ -22,3 +28,7 @@ while run:
             run = False
 
     pygame.draw.circle(wn, ORANGE, (ball_x, ball_y), radius)
+    pygame.draw.rect(wn, BLUE, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
+    pygame.draw.rect(wn, BLUE, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
+    
+    pygame.display.update()
